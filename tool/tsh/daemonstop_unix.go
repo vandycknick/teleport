7804c2f286365e1cb0b5ad301e6d344a5fff0ba1
Rafał Cieślak <rafal.cieslak@goteleport.com>
2023-01-16 15:33:26 +0100

    Add comment for onDaemonStop

--- tool/tsh/daemonstop_unix.go
+++ tool/tsh/daemonstop_unix.go
@@ -3,6 +3,8 @@
 
 package main
 
+// onDaemonStop implements the "tsh daemon stop" command. It handles graceful shutdown of the daemon
+// on Windows, so it's a noop on other platforms. See daemonstop_windows.go for more details.
 func onDaemonStop(cf *CLIConf) error {
 	return nil
 }
